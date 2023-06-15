2e0643df4a5a4af7e18dfb7fb3c13e4b647a9c54
Dag7m <dagemgirum@gmail.com>
2023-06-15 00:12:48 -0700

    hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - Where the program starts
+ *returns 0 means no error
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
