dcb8cde27b840f7ec8982c5247d23b70bd345de6
Dag7m <dagemgirum@gmail.com>
2023-06-16 02:46:54 -0700

    cc

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers that are below 10 (integers)
+ *
+ * Return: Always 0 no error
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
+
