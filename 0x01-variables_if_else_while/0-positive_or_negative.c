a70e3de9ab187c6eaf951caffbcbbdd3766ac321
Dag7m <dagemgirum@gmail.com>
2023-06-16 02:31:07 -0700

    Betty error fix

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,11 +2,12 @@
 #include <time.h>
 /* more headers goes there */
 #include <stdio.h>
-/* betty style doc for function main goes there */
-/*
- * main - Determines if a number is positive, negative or zero
- * Return : 0 is success
-*/
+/**
+ * main - Determines if a number is positive, negative or zero.
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	int n;
