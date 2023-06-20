eec0f216fd004f80393f416d5067fc745404f852
Dag7m <dagemgirum@gmail.com>
2023-06-20 03:34:09 -0700

    wow

--- 0x02-functions_nested_loops/10-main.c
+++ 0x02-functions_nested_loops/10-main.c
@@ -1,19 +1,16 @@
 #include "main.h"
+#include <stdio.h>
 
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int n;
 
-	/**
-	 * add - adds two integers and returns the result
-	 * @a: int to be added to b
-	 * @b: int to be added to a
-	 * Return: sum of a and b
-	 */
-	int add(int a, int b)
-	{
-		int c;
-
-
-		c = a + b;
-
-
-		return (c);
-	}
+    n = add(89, 9);
+    printf("%d\n", n);
+    return (0);
+}
